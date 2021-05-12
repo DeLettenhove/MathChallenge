1ed0d070b908f9a26f2e677d03b7ef837ff3c140
DeLettenhove <deletten@gmail.com>
2021-05-12 11:33:14 +0300

    MathChalenge function created

---
+++ js/app.js
@@ -0,0 +1,89 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  result.innerText = `Result: ${MathChallenge(input.value)}`;
+});
+
+const highRegExp = /\(\d+[\+\-\*\/]\d+\)/;
+const midRegExp = /\d+[\*\/]\d+/;
+const lowRegExp = /\d+[\+\-]\d+/;
+
+function MathChallenge(str) {
+  while (isNaN(Number(str))) {
+    if (str.includes('(') || str.includes(')')) {
+      str = calc(highRegExp, str);
+    } else if (str.includes('*') || str.includes('/')) {
+      str = calc(midRegExp, str);
+    } else if (str.includes('+') || str.includes('-')) {
+      str = calc(lowRegExp, str);
+    }
+  }
+  return str;
+}
+
+function calc(regExp, str) {
+  let match = str.match(regExp)[0];
+  if (regExp === highRegExp) {
+    quoteMatch = match;
+    match = removeQuotes(match);
+  }
+  let calcRes = operation(match);
+  if (regExp === highRegExp) {
+    match = quoteMatch;
+  }
+  str = str.replace(match, calcRes);
+  return str;
+}
+
+function removeQuotes(str) {
+  let oq = str.indexOf('(') + 1;
+  let cq = str.indexOf(')');
+  str = str.slice(oq, cq);
+  return str;
+}
+
+function operation(str) {
+  let res;
+  let arr = getOperands(str);
+  let [a, op, b] = arr;
+  switch (op) {
+    case '+':
+      res = Number(a) + Number(b);
+      return res;
+    case '-':
+      res = Number(a) - Number(b);
+      return res;
+    case '*':
+      res = Number(a) * Number(b);
+      return res;
+    case '/':
+      res = Number(a) / Number(b);
+      return res;
+  }
+}
+
+function getOperands(str) {
+  let a, op, b, res;
+  let arr = str.split('');
+  if (arr.length === 3) {
+    return arr;
+  } else {
+    arr.forEach((item) => {
+      if (isNaN(Number(item))) {
+        op = item;
+        [a, b] = str.split(op);
+        res = [a, op, b];
+      }
+    });
+  }
+  return res;
+}
+
+console.log(MathChallenge('12*3'));
+console.log(MathChallenge('6*(4/2)+3*1'));
+console.log(MathChallenge('7*7'));
+console.log(MathChallenge('(8-7)+2*7'));
+console.log(MathChallenge('6*8+(7-1)*3-(2*2)/2-7'));
+console.log(MathChallenge('123*321'));
